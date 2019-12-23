d9f77b0631ea3120057f6c3acc545d0f46278028
DaNaRim <rangar.danarim@gmail.com>
2019-12-23 02:02:44 +0200

    lesson11 Homework done

--- src/lesson11/homework/Demo.java
+++ src/lesson11/homework/Demo.java
@@ -7,10 +7,10 @@ public class Demo {
     public static void main(String[] args) {
 
         Room[] rooms = new Room[5];
-        rooms[0] = new Room(1478562, 50, 1, new Date(), "Orange", "Cherkassy");
-        rooms[1] = new Room(1278671, 150, 2, new Date(), "Orange", "Cherkassy");
-        rooms[2] = new Room(1678621, 250, 3, new Date(), "Orange", "Cherkassy");
-        rooms[3] = new Room(2786622, 1, 4, new Date(), "Orange", "Cherkassy");
+        rooms[0] = new Room(478662, 50, 1, new Date(), "Orange", "Cherkassy");
+        rooms[1] = new Room(257861, 150, 2, new Date(), "Orange", "Cherkassy");
+        rooms[2] = new Room(678621, 250, 3, new Date(), "Orange", "Cherkassy");
+        rooms[3] = new Room(278672, 1, 4, new Date(), "Orange", "Cherkassy");
         rooms[4] = null;
         Room[] rooms2 = new Room[5];
         rooms2[0] = new Room(122751, 50, 1, new Date(), "Orange", "Cherkassy");
@@ -30,18 +30,36 @@ public class Demo {
         BookingComAPI bookingComAPI = new BookingComAPI(rooms3);
 
         Controller controller = new Controller(new API[]{googleAPI, tripAdvisorAPI, null});
-        System.out.println(Arrays.toString(controller.check(googleAPI, tripAdvisorAPI)));
-        System.out.println(Arrays.toString(controller.requestRooms(50, 2, "Cherkassy", "Orange")));
+        for (Room room : controller.check(googleAPI, tripAdvisorAPI)) {
+            System.out.println(room.getId());
+        }
+
+        System.out.println();
+
+        for (Room room : controller.requestRooms(50, 2, "Cherkassy", "Orange")) {
+            System.out.println(room.getId());
+        }
+
+        System.out.println();
 
         TripAdvisorAPI tripAdvisorAPI1 = new TripAdvisorAPI(rooms);
-        System.out.println(Arrays.toString(tripAdvisorAPI1.findRooms(100, 2, "Cherkassy", "Orange")));
+        for (Room room : tripAdvisorAPI1.findRooms(100, 2, "Cherkassy", "Orange")) {
+            System.out.println(room.getId());
+        }
+
+        System.out.println();
 
         GoogleAPI googleAPI1 = new GoogleAPI(rooms);
-        System.out.println(Arrays.toString(googleAPI1.findRooms(150, 2, "Cherkassy", "Orange")));
+        for (Room room : googleAPI1.findRooms(150, 2, "Cherkassy", "Orange")) {
+            System.out.println(room.getId());
+        }
+
+        System.out.println();
 
         BookingComAPI bookingComAPI1 = new BookingComAPI(rooms);
-        System.out.println(Arrays.toString(bookingComAPI1.findRooms(150, 2, "Cherkassy", "Orange")));
-
+        for (Room room : bookingComAPI1.findRooms(150, 2, "Cherkassy", "Orange")) {
+            System.out.println(room.getId());
+        }
 
     }
 }
