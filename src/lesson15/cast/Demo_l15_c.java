592ff9143e575690fcccf76632781ddbdcd4931b
DaNaRim <rangar.danarim@gmail.com>
2019-12-28 23:23:35 +0200

    lesson15

--- src/lesson15/cast/Demo_l15_c.java
+++ src/lesson15/cast/Demo_l15_c.java
@@ -1,12 +1,10 @@
 package lesson15.cast;
 
-public class Demo {
+public class Demo_l15_c {
     public static void main(String[] args) {
 
         InternetProvider provider = (InternetProvider) test();
         FoodProvider foodProvider = (FoodProvider) testFood();
-
-
 
         System.out.println(provider);
     }
