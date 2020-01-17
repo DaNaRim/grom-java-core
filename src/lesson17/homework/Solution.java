34438f7935d3edd532abb927f2b13774e0234ca6
DaNaRim <rangar.danarim@gmail.com>
2020-01-17 22:33:57 +0200

    Lesson 17 homework

--- src/lesson17/homework/Solution.java
+++ src/lesson17/homework/Solution.java
@@ -1,13 +1,77 @@
 package lesson17.homework;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(countWords("    AAA    BBB CCC"));
+    }
 
+    public static int countWords(String input) {
+        if (input == null) return 0;
 
-    public int countWords(String input) {
-        if (input == null || input.isEmpty()) return 0;
+        int result = 0;
+        for (String str : input.trim().split(" ")) {
+            if (!str.equals("") && checkWord(str)) result++;
+        }
+        return result;
+    }
 
-        String[] words = input.split(" ");
+    public static String maxWord(String input) {
+        if (input == null) return null;
 
-        return words.length;
+        String resultWord = "";
+
+        for (String str : input.trim().split(" ")) {
+            if (!str.equals("") && checkWord(str)) {
+                if (resultWord.length() < str.length()) resultWord = str;
+            }
+        }
+        return resultWord;
+    }
+
+    public static String minWord(String input) {
+        if (input == null) return null;
+
+        String resultWord = input.trim().split(" ")[0];
+
+        for (String str : input.trim().split(" ")) {
+            if (!str.equals("") && checkWord(str)) {
+                if (resultWord.length() > str.length()) resultWord = str;
+            }
+        }
+        return resultWord;
+    }
+
+    public static String mostCountedWord(String input) {
+        if (input == null) return null;
+
+        String resultWord = "";
+        int maxNumOfRepWord = 0;
+
+        for (String str : input.trim().split(" ")) {
+            if (countDuplicates(input, str) > maxNumOfRepWord) {
+                resultWord = str;
+                maxNumOfRepWord = countDuplicates(input, str);
+            }
+        }
+        return resultWord.equals("") ? null : resultWord;
+    }
+
+    private static boolean checkWord(String word) {
+        for (char checkChar : word.toCharArray()) {
+            if (/*!чекаєм чи буква це*/) return false;
+        }
+        return true;
+    }
+
+    private static int countDuplicates(String input, String word) {
+        String[] strings = input.split(" ");
+
+        int res = 0;
+        for (String string : strings) {
+            if (string.equals(word)) res++;
+        }
+        return res;
     }
 }
