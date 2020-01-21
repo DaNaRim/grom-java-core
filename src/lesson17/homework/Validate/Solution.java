1a10135f473fca710beddc0db02f3751079d0fd8
DaNaRim <57962534+DaNaRim@users.noreply.github.com>
2020-01-21 18:39:17 +0200

    Update Solution.java

--- src/lesson17/homework/Validate/Solution.java
+++ src/lesson17/homework/Validate/Solution.java
@@ -1,54 +1,21 @@
 package lesson17.homework.Validate;
 
 public class Solution {
-  public class Solution {
-    public static String validate(String address) {
-        if (address == null) return null;
+    public static boolean validate(String address) {
+        if (address == null || address.isEmpty()) return false;
 
+        if (!address.startsWith("http://") && !address.startsWith("https://"))
+            return false;
 
-        for (int i = 0; i < address.toCharArray().length; i++) {
-            if (!Character.isLetter(i)){
-                address = address.replace(i, "");
-            }
-        }
+        if (!address.endsWith(".com") && !address.endsWith(".org") && !address.endsWith(".net"))
+            return false;
 
+        address = address.substring(0, address.length() - 3);
 
         for (char ch : address.toCharArray()) {
-            if (!Character.isLetter(ch))
-
-                address = address.replace(ch, );
+            if (!Character.isLetter(ch) && !Character.isDigit(ch))
+                return false;
         }
-
-
-
-        if (!address.startsWith("http://") || !address.startsWith("https://")){
-            address = "http://" + address;
-            
-            if (address.endsWith(".com") || address.endsWith(".org") || address.endsWith(".net")){
-
-            }
-        }
-
-        String subString = address.substring(address.length() - 2, address.length() + 1);
-
-        if (subString.equals("com") || subString.equals("org") || subString.equals("net")) {
-            if (!address.substring(address.length() - 3, address.length() - 2).equals(".")) {
-                String firstString = address.substring(0, address.length() - 3);
-                String thirdString = address.substring(address.length() - 2);
-                address = firstString + "." + thirdString;
-            } else {
-                if (!address.substring(address.length() - 3, address.length() - 2).equals("."))
-                    address += ".com";
-                else
-                    address += "com";
-            }
-        }
-
-
-
-        if (!address.substring(address.length() - 3, address.length() - 2).equals("."))
-            address += ".";
-        char[] ch = address.trim().toCharArray();
+        return true;
     }
-   
 }
