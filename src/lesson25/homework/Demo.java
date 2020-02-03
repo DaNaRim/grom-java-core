d5b184e0a9a7911fc46f257a70c41033967013d2
DaNaRim <rangar.danarim@gmail.com>
2020-02-03 21:46:06 +0200

    Lesson 25 homework

---
+++ src/lesson25/homework/Demo.java
@@ -0,0 +1,66 @@
+package lesson25.homework;
+
+import java.util.Arrays;
+
+public class Demo {
+
+    private static class Test1 {
+        private int testInt;
+
+        public Test1(int testInt) {
+            this.testInt = testInt;
+        }
+    }
+
+    private static class Test2 {
+        private final String testString = "test";
+        private int testInt = 0;
+
+        public Test2(int testInt) {
+            this.testInt = testInt;
+        }
+    }
+
+    private static class Test3 {
+        private long testLong;
+        private short testShort;
+        private String testString;
+
+        public Test3(long testLong, short testShort, String testString) {
+            this.testLong = testLong;
+            this.testShort = testShort;
+            this.testString = testString;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1(0);
+        Test2 test2 = new Test2(2134);
+        Test3 test3 = new Test3(444, Short.MAX_VALUE, "111");
+
+        GeneralDAO<Test1> gTest1 = new GeneralDAO<>();
+        try {
+            gTest1.save(test1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(Arrays.toString(gTest1.getAll()));
+
+        GeneralDAO<Test2> gTest2 = new GeneralDAO<>();
+        try {
+            gTest2.save(test2);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(Arrays.toString(gTest2.getAll()));
+
+        GeneralDAO<Test3> gTest3 = new GeneralDAO<>();
+        try {
+            gTest3.save(test3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(Arrays.toString(gTest3.getAll()));
+
+    }
+}
---
+++ src/lesson25/homework/GeneralDAO.java
@@ -0,0 +1,36 @@
+package lesson25.homework;
+
+public class GeneralDAO<T> {
+    private T[] database = (T[]) new Object[10];
+
+    public T save(T t) throws Exception {
+        validate(t);
+        int index = 0;
+        for (T el : database) {
+            if (el == null) {
+                database[index] = t;
+                return database[index];
+            }
+            index++;
+        }
+        return null;
+    }
+
+    public T[] getAll() {
+        return database;
+    }
+
+    private void validate(T t) throws Exception {
+        if (t == null) throw new Exception("Object is empty");
+
+        for (T el : database) {
+            if (t.equals(el)) throw new Exception("The object is already in the database");
+        }
+
+        int count = 0;
+        for (T el : database) {
+            if (el != null) count++;
+        }
+        if (count == database.length) throw new Exception("There is no space for the object");
+    }
+}
