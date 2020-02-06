821fd5d1dd3eabf5ffe4a650cb0c6405ed1a38d8
DaNaRim <rangar.danarim@gmail.com>
2020-02-06 11:24:45 +0200

    Lesson 28

---
+++ src/lesson28/DemoComparator.java
@@ -0,0 +1,48 @@
+package lesson28;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+
+public class DemoComparator {
+    public static void main(String[] args) {
+        Capability capability2 = new Capability(1005, "test", "rrr", false, new Date());
+        Capability capability4 = new Capability(900, "test", "rrr", false, new Date());
+        Capability capability3 = new Capability(900, "test", "rrr", true, new Date());
+        Capability capability1 = new Capability(1001, "test", "rrr", true, new Date());
+
+        ArrayList<Capability> capabilities = new ArrayList<>();
+        capabilities.add(capability1);
+        capabilities.add(capability2);
+        capabilities.add(capability3);
+        capabilities.add(capability4);
+
+        System.out.println(capabilities);
+
+        capabilities.sort(new IsActiveComparator());
+
+        System.out.println(capabilities);
+
+
+        Comparator<Capability> comparator = new Comparator<Capability>() {
+            @Override
+            public int compare(Capability capability, Capability t1) {
+
+                // если chanelName не равно - сравниваю по нему
+                //если равно - перехожу к fingerprint
+                //если fingerprint не равно - сравниваю по нему
+                //если равно перехожу к dateCreated
+                //если dateCreated не равно - сравниваю по нему
+                //если равно - объекты равны
+
+
+                if (!capability.getChanelName().equals(t1))
+                    return capability.getChanelName().compareTo(t1.getChanelName());
+
+                //TODO
+
+                return 0;
+            }
+        };
+    }
+}
---
+++ src/lesson28/IsActiveComparator.java
@@ -0,0 +1,18 @@
+package lesson28;
+
+import java.util.Comparator;
+
+public class IsActiveComparator implements Comparator<Capability> {
+
+    @Override
+    public int compare(Capability o1, Capability o2) {
+        System.out.println("IsActiveComparator is used");
+        if (o1.isActive() && !o2.isActive())
+            return -1;
+        else if (o1.isActive() && o2.isActive() || !o1.isActive() && !o2.isActive())
+            return 0;
+        else
+            return 1;
+    }
+}
+
