202d256fb3516f0eeefaa9080c82e190cf4ece26
DaNaRim <rangar.danarim@gmail.com>
2020-02-07 12:43:27 +0200

    Lesson 28 homework

---
+++ src/lesson29/File.java
@@ -0,0 +1,38 @@
+package lesson29;
+
+import java.util.Objects;
+
+public class File {
+    private String fileName;
+    private long sizeInBytes;
+
+    public File(String fileName, long sizeInBytes) {
+        this.fileName = fileName;
+        this.sizeInBytes = sizeInBytes;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        File file = (File) o;
+        return Objects.equals(fileName, file.fileName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileName);
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "fileName='" + fileName + '\'' +
+                ", sizeInBytes=" + sizeInBytes +
+                '}';
+    }
+}
---
+++ src/lesson29/homework/HashSetTest.java
@@ -0,0 +1,40 @@
+package lesson29.homework;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetTest {
+
+    public Set<Order> useHashSet() {
+        Set<Order> testSet = new HashSet<>();
+        Set<Order> testSet2 = new HashSet<>();
+
+        Order order1 = new Order(123444, 444, "USD", "Hammer", "aseg23wgw");
+        Order order2 = new Order(123555, 555, "USD", "Hammer", "sdbh33322");
+
+        testSet.add(order1);
+
+        testSet.remove(order1);
+
+        testSet.add(order1);
+        testSet.add(order2);
+        testSet2.add(order1);
+        testSet.retainAll(testSet2);
+
+        testSet.toArray();
+
+        testSet.size();
+
+        Iterator<Order> fileIterator = testSet.iterator();
+
+        fileIterator.next();
+
+        for (int i = 0; i < 5; i++) {
+            Order superOrder = new Order(i * 123, i * 100, "USD", "Hammer" + i, "sdbh33322" + i * 5);
+            testSet.add(superOrder);
+        }
+        return testSet;
+    }
+}
+
