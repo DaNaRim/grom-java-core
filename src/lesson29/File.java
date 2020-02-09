7101b0519dc81fa4bacd2c161e49f05bd1332981
DaNaRim <rangar.danarim@gmail.com>
2020-02-09 13:30:36 +0200

    Lesson 30

--- src/lesson29/File.java
+++ src/lesson29/File.java
@@ -2,7 +2,7 @@ package lesson29;
 
 import java.util.Objects;
 
-public class File {
+public class File implements Comparable<File> {
     private String fileName;
     private long sizeInBytes;
 
@@ -13,6 +13,22 @@ public class File {
 
     public String getFileName() {
         return fileName;
+    }
+
+    public long getSizeInBytes() {
+        return sizeInBytes;
+    }
+
+    @Override
+    public int compareTo(File file) {
+        int res = 0;
+
+        if (file.getSizeInBytes() > this.sizeInBytes)
+            res = -1;
+        else if (file.getSizeInBytes() < this.sizeInBytes)
+            res = 1;
+
+        return res;
     }
 
     @Override
