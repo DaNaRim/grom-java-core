b3fdf808d47c7551e10d799bbb9a0f27c704925b
DaNaRim <rangar.danarim@gmail.com>
2020-02-09 23:20:08 +0200

    Lesson 31 homework

---
+++ src/lesson31/homework/Demo.java
@@ -0,0 +1,17 @@
+package lesson31.homework;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(Solution.countSymbols("fudshviosdnvlsdnmcoabsdfibalsefb"));
+        System.out.println(Solution.countSymbols("ggggsajhakhajjjjalsd"));
+        System.out.println(Solution.countSymbols("         ggggsajh           akhajjjjalsd"));
+        System.out.println(Solution.countSymbols("ddddddddddddddddddddd"));
+        System.out.println(Solution.countSymbols("                 "));
+
+        System.out.println(Solution.words("333  awsef tess wsf tess awsef"));
+        System.out.println(Solution.words(" fd gs    segf   wedd wedd"));
+        System.out.println(Solution.words("              "));
+        System.out.println(Solution.words(" 2134 5719 2065"));
+        System.out.println(Solution.words("rrr rrr rrr ddd rrr ddd rrr"));
+    }
+}
