55462b89743673aecbc9b6b6852c523a9064a055
DaNaRim <rangar.danarim@gmail.com>
2020-02-11 17:19:50 +0200

    Lesson 28 homework

--- src/lesson28/task2/FullComparator.java
+++ src/lesson28/task2/FullComparator.java
@@ -3,18 +3,26 @@ package lesson28.task2;
 import java.util.Comparator;
 
 public class FullComparator implements Comparator<Capability> {
-
     @Override
     public int compare(Capability capability, Capability t1) {
+
+        if (capability.getChanelName() == null) return 1;
+        if (t1.getChanelName() == null) return -1;
+
         if (!capability.getChanelName().equals(t1.getChanelName()))
             return capability.getChanelName().compareTo(t1.getChanelName());
+
+        if (capability.getFingerPrint() == null) return 1;
+        if (t1.getFingerPrint() == null) return -1;
 
         if (!capability.getFingerPrint().equals(t1.getFingerPrint()))
             return capability.getFingerPrint().compareTo(t1.getFingerPrint());
 
-        if (!capability.getDateCreated().equals(t1.getDateCreated()))
-            return capability.getDateCreated().compareTo(t1.getDateCreated());
+        if (capability.getDateCreated() == null) return 1;
+        if (t1.getDateCreated() == null) return -1;
 
+        if (!t1.getDateCreated().equals(capability.getDateCreated()))
+            return t1.getDateCreated().compareTo(capability.getDateCreated());
         return 0;
     }
 }
