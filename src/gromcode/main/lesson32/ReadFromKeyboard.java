14c50a202bfd3c6bbc34818afc01a7c6684b7095
DaNaRim <rangar.danarim@gmail.com>
2022-03-13 00:06:36 +0200

    code cleanup: lessons32

--- src/gromcode/main/lesson32/ReadFromKeyboard.java
+++ src/gromcode/main/lesson32/ReadFromKeyboard.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ReadFromKeyboard {
+
     public static void main(String[] args) throws IOException {
         readKeyboardWithIOStream();
     }
--- src/gromcode/main/lesson32/homework/Demo.java
+++ src/gromcode/main/lesson32/homework/Demo.java
@@ -1,6 +1,7 @@
 package gromcode.main.lesson32.homework;
 
 public class Demo {
+
     public static void main(String[] args) {
         System.out.println(Solution.readNumbers());
     }
